7074741d7c65ac65989e90d74b39b90dbf322323
TheJPMZ <monzon.jpmz@gmail.com>
2022-11-07 17:26:50 -0600

    🔱 Merge Integration tests

--- components/ScreenGuias/__tests__/Calificacion.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Calificacion from '../../ScreenGuias/Calificacion';
-
-test('renders correctly', () => {
-  const tree = renderer.create(<Calificacion />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
