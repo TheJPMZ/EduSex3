14c811cc801a01b42c40a31a35572e40e2e0f232
Estef072 <61289015+Estef072@users.noreply.github.com>
2022-11-07 14:29:07 -0600

    TERMINADO:🎉🎉🎉🎉

--- cypress/e2e/Preservativos.cy.js
+++ cypress/e2e/Preservativos.cy.js
@@ -1,9 +1,71 @@
 describe('Test-Prservativos', () => {
-    it('prueba del cambio de preservativos', () =>{
+    it('prueba anillo Vaginal', () =>{
         cy.visit('http://localhost:19007/')
         cy.contains('OTRO').click()
         cy.contains('Preservativos').click()
         cy.contains('Anillo Vaginal').click()
+    }) 
+
+    it('prueba Capuchón Cervical', () =>{
+        cy.visit('http://localhost:19007/')
+        cy.contains('MASCULINO').click()
+        cy.contains('Preservativos').click()
+        cy.contains('Capuchón Cervical').click()
+    }) 
+
+    it('prueba Condón Femenino', () =>{
+        cy.visit('http://localhost:19007/')
+        cy.contains('FEMENINO').click()
+        cy.contains('Preservativos').click()
+        cy.contains('Condón Femenino').click()
+    }) 
+
+    it('prueba Condón Masculino', () =>{
+        cy.visit('http://localhost:19007/')
+        cy.contains('OTRO').click()
+        cy.contains('Preservativos').click()
+        cy.contains('Condón Masculino').click()
+    }) 
+
+    it('prueba Diafragma', () =>{
+        cy.visit('http://localhost:19007/')
+        cy.contains('MASCULINO').click()
+        cy.contains('Preservativos').click()
+        cy.contains('Diafragma').click()
+    }) 
+
+    it('prueba  Espermicida y Gel', () =>{
+        cy.visit('http://localhost:19007/')
+        cy.contains('FEMENINO').click()
+        cy.contains('Preservativos').click()
+        cy.contains('Espermicida y Gel').click()
+    }) 
+
+    it('prueba  Esponja Anticonceptiva', () =>{
+        cy.visit('http://localhost:19007/')
+        cy.contains('OTRO').click()
+        cy.contains('Preservativos').click()
+        cy.contains('Esponja Anticonceptiva').click()
+    }) 
+
+    it('prueba Inyección Anticonceptiva', () =>{
+        cy.visit('http://localhost:19007/')
+        cy.contains('MASCULINO').click()
+        cy.contains('Preservativos').click()
+        cy.contains('Inyección Anticonceptiva').click()
+    }) 
+    it('prueba  Parche Anticonceptivo', () =>{
+        cy.visit('http://localhost:19007/')
+        cy.contains('FEMENINO').click()
+        cy.contains('Preservativos').click()
+        cy.contains('Parche Anticonceptivo').click()
+    }) 
+
+    it('prueba Pastilla', () =>{
+        cy.visit('http://localhost:19007/')
+        cy.contains('OTRO').click()
+        cy.contains('Preservativos').click()
+        cy.contains('Pastilla').click()
     }) 
 
 })
